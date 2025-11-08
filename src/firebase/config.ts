78cdee6ac5aae9d55ee347799d62040ce64d019a
Rohit Verma <rohitvetma101010@gmail.com>
2025-11-08 17:44:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "billtrack-pro",
+  "appId": "1:918920482258:web:d636df7d2a45b578f40c0b",
+  "apiKey": "AIzaSyAs8g-lxE9YbM_7RtPWu0MhmF-5seG9dEA",
+  "authDomain": "billtrack-pro.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "918920482258"
+};
